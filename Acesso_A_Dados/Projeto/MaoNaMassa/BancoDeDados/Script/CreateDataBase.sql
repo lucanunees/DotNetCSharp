a1f229b2a4b7a5d1c41e6911d5eb1e7c6b5fe7b7
Lucas Nunes o/ <lucasrafaelnunees@gmail.com>
2023-03-23 01:35:19 -0300

    Finalizando o modulo de Imersão e Iniciando o MaoNaMassa Aula DapperContrib

---
+++ Acesso_A_Dados/Projeto/MaoNaMassa/BancoDeDados/Script/CreateDataBase.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE [Blog]
+GO
+
+USE [BLOG]
+GO
---
+++ Acesso_A_Dados/Projeto/MaoNaMassa/Projeto/Blog/Program.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog;
+class Program
+{
+    private const string CONNECTION_STRING = @"Server=localhost,1433;DataBase=Blog;User ID=sa;Password=Punto@2015";
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static void ReadUser()
+    {
+        using(var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var users = connection.GetAll<User>();
+
+            foreach (var item in users)
+            {
+                Console.WriteLine($"{item.Name}");
+            }
+        }
+    }
+}
