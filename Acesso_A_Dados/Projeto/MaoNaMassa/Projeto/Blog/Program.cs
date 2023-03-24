ef5d923320db927ed8f7d4c17d7fa6de960903a5
Lucas Nunes o/ <lucasrafaelnunees@gmail.com>
2023-03-24 01:23:32 -0300

    Aula Dapper Contrib e Aula CRUD

--- Acesso_A_Dados/Projeto/MaoNaMassa/Projeto/Blog/Program.cs
+++ Acesso_A_Dados/Projeto/MaoNaMassa/Projeto/Blog/Program.cs
@@ -5,22 +5,98 @@ using Microsoft.Data.SqlClient;
 namespace Blog;
 class Program
 {
-    private const string CONNECTION_STRING = @"Server=localhost,1433;DataBase=Blog;User ID=sa;Password=Punto@2015";
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=Punto@2015;Trusted_Connection=False; TrustServerCertificate=True;";
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // ReadUsers();
+        // ReadUser();
+        // CreateUser();
+        // UpdateUser();
+        DeleteUser();
+    }
+
+    /* ================ CRUD ================ */
+    public static void CreateUser()
+    {
+        // Para inserir um usuario, eu preciso criar um objeto usuario.
+        var user = new User()
+        {
+            Bio = "Equipe lucas.io",
+            Email = "Equipe@dev.io",
+            Image = "Https://",
+            Name = "Equipe lucas.io",
+            PasswordHash = "HASH",
+            Slug = "Equipe-lucas"
+        };
+
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            //Ele retorna um inteiro, com a quantidade de linhas afetas.
+            var rows = connection.Insert<User>(user);
+            {
+                Console.WriteLine($"Cadastro realizado com sucesso. Linhas inseridas - {rows}.");
+            }
+        }
     }
 
     public static void ReadUser()
     {
-        using(var connection = new SqlConnection(CONNECTION_STRING))
+        using (var connection = new SqlConnection(CONNECTION_STRING))
         {
+            //Aqui iremos pegar somente 1 usuario cujo o ID é = 1   
+            var user = connection.Get<User>(1);
+            Console.WriteLine($"{user.Name}");
+        }
+    }
+
+    public static void ReadUsers()
+    {
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            // Aqui ele já realiza o select * na tabela,
+            // porém ficar espero porque ele pluraliza a tabela ou seja a tabela esta User, ele vai buscar como User
+            // para corrigir isso precisa realizar uma modificação na models.
             var users = connection.GetAll<User>();
 
-            foreach (var item in users)
+            foreach (var user in users)
             {
-                Console.WriteLine($"{item.Name}");
+                Console.WriteLine($"{user.Name}");
             }
         }
     }
+
+    public static void UpdateUser()
+    {
+        var userUpdate = new User()
+        {
+            Id = 3,
+            Bio = "Equipe de dev lucas.io",
+            Email = "Equipe@dev.io",
+            Image = "Https://",
+            Name = "Equipe de dev lucas.io",
+            PasswordHash = "HASH",
+            Slug = "Equipe-lucas"
+        };
+
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var result = connection.Update<User>(userUpdate);
+            {
+                Console.WriteLine($"Atualização com sucesso. Resultado do update - {result}.");
+            }
+        }
+    }
+
+    public static void DeleteUser()
+    {
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var userDelete = connection.Get<User>(3);
+            var result = connection.Delete<User>(userDelete);
+            {
+                Console.WriteLine($"Usuario deletado com sucesso. Resultado - {result}");
+            }
+        }
+    }
+
 }
